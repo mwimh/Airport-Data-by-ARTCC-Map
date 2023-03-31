e9e4a3e5101f2630550a58371a32aa1d619b49ff
mwimh <112586751+mwimh@users.noreply.github.com>
2023-03-31 11:36:56 -0500

    Activity 9 Complete
    Unit 3 - Activity 9 Complete

--- js/main.js
+++ js/main.js
@@ -1,155 +1,105 @@
-//execute script when window is loaded
-window.onload = function () {
-    //SVG dimension variables
-    var w = 900, h = 500;
+//begin script when window loads
+window.onload = setMap();
 
-    //Example 1.2 line 1...container block
-    var container = d3.select("body") //get the <body> element from the DOM
-        .append("svg") //put a new svg in the body
-        .attr("width", w) //assign the width
-        .attr("height", h) //assign the height
-        .attr("class", "container") //assign a class name
-        .style("background-color", "rgba(0,0,0,0.2)"); //svg background color
+function setMap() {
 
-    //innerRect block
-    var innerRect = container.append("rect") //put a new rect in the svg
-        .datum(400) //a single value is a datum
-        .attr("width", function (d) { //rectangle width
-            return d * 2; //400 * 2 = 800
-        })
-        .attr("height", function (d) { //rectangle height
-            return d; //400
-        })
-        .attr("class", "innerRect") //class name
-        .attr("x", 50) //position from left on the x (horizontal) axis
-        .attr("y", 50) //position from top on the y (vertical) axis
-        .style("fill", "#FFFFFF"); //fill color
+    //map frame dimensions
+    var width = 960,
+        height = 550;
 
-    //below Example 1.9
-    var cityPop = [
-        {
-            city: 'Madison',
-            population: 233209
-        },
-        {
-            city: 'Milwaukee',
-            population: 594833
-        },
-        {
-            city: 'Green Bay',
-            population: 104057
-        },
-        {
-            city: 'Superior',
-            population: 27244
-        }
-    ];
+    //create new svg container for the map
+    var map = d3.select("body")
+        .append("svg")
+        .attr("class", "map")
+        .attr("width", width)
+        .attr("height", height);
 
-    //find the minimum value of the array
-    var minPop = d3.min(cityPop, function (d) {
-        return d.population;
-    });
+    //create Albers equal area conic projection centered on the continental United States
+    var projection = d3.geoAlbers()
+        .center([0, 38.5])
+        .rotate([98.5, 0, 0])
+        .parallels([20, 45])
+        .scale(1050)
+        .translate([width / 2, height / 2]);
 
-    //find the maximum value of the array
-    var maxPop = d3.max(cityPop, function (d) {
-        return d.population;
-    });
+    var path = d3.geoPath()
+        .projection(projection);
 
-    //color scale generator 
-    var color = d3.scaleLinear()
-        .range([
-            "#FDBE85",
-            "#D94701"
-        ])
-        .domain([
-            minPop,
-            maxPop
-        ]);
+    //use Promise.all to parallelize asynchronous data loading
+    var promises = [];
+    promises.push(d3.csv("data/ARTCCData.csv")); //load attributes from csv    
+    promises.push(d3.json("data/ARTCCs.topojson")); //load choropleth spatial data    
+    promises.push(d3.json("data/CONUS.topojson")); //load state overlays spatial data
+    promises.push(d3.json("data/BackgroundCountries.topojson")); //load background country spatial data 
+    promises.push(d3.json("data/points.topojson")); //load reference city points
+    Promise.all(promises).then(callback);
 
-    //scale for circles center y coordinate
-    var y = d3.scaleLinear()
-        .range([450, 50]) //was 440, 95
-        .domain([0, 700000]); //was minPop, maxPop
+    //assign data to variables
+    function callback(data) {
+        csvData = data[0];
+        artccsData = data[1];
+        conusData = data[2];
+        bGCount = data[3];
+        cntrPoints = data[4];
 
-    var x = d3.scaleLinear() //create the scale
-        .range([90, 750]) //output min and max
-        .domain([0, 3]); //input min and max
+        //translate spatial data back to TopoJSON
+        var centersTopo = topojson.feature(artccsData, artccsData.objects.ARTCCs).features,
+            statesTopo = topojson.feature(conusData, conusData.objects.CONUS),
+            backCount = topojson.feature(bGCount, bGCount.objects.BackgroundCountries),
+            pointsTopo = topojson.feature(cntrPoints, cntrPoints.objects.points);
 
-    //Example 2.6 line 3
-    var circles = container.selectAll(".circles") //create an empty selection
-        .data(cityPop) //here we feed in an array
-        .enter() //one of the great mysteries of the universe
-        .append("circle") //inspect the HTML--holy crap, there's some circles there
-        .attr("class", "circles")
-        .attr("id", function (d) {
-            return d.city;
-        })
-        .attr("r", function (d) {
-            //calculate the radius based on population value as circle area
-            var area = d.population * 0.01;
-            return Math.sqrt(area / Math.PI);
-        })
-        .attr("cx", function (d, i) {
-            //use the scale generator with the index to place each circle horizontally
-            return x(i);
-        })
-        .attr("cy", function (d) {
-            return y(d.population);
-        })
-        .style("fill", function (d, i) { //add a fill based on the color scale generator
-            return color(d.population);
-        })
-        .style("stroke", "#000"); //black circle stroke
+        //create graticule generator
+        var graticule = d3.geoGraticule()
+            .step([5, 5]); //place graticule lines every 5 degrees of longitude and latitude
 
-    var yAxis = d3.axisLeft(y);
+        //create graticule background
+        var gratBackground = map.append("path")
+            .datum(graticule.outline()) //bind graticule background
+            .attr("class", "gratBackground") //assign class for styling
+            .attr("d", path) //project graticule
 
-    //create axis g element and add axis
-    var axis = container.append("g")
-        .attr("class", "axis")
-        .attr("transform", "translate(50, 0)")
-        .call(yAxis);
+        //create graticule lines
+        var gratLines = map.selectAll(".gratLines") //select graticule elements that will be created
+            .data(graticule.lines()) //bind graticule lines to each element to be created
+            .enter() //create an element for each datum
+            .append("path") //append each element to the svg as a path element
+            .attr("class", "gratLines") //assign class for styling
+            .attr("d", path); //project graticule lines
 
-    var title = container.append("text")
-        .attr("class", "title")
-        .attr("text-anchor", "middle")
-        .attr("x", 450)
-        .attr("y", 30)
-        .text("City Populations!");
+        //add background countries to map
+        var countries = map.append("path")
+            .datum(backCount)
+            .attr("class", "backgnd")
+            .attr("d", path);
 
-    var labels = container.selectAll(".labels")
-        .data(cityPop)
-        .enter()
-        .append("text")
-        .attr("class", "labels")
-        .attr("text-anchor", "left")
-        .attr("y", function (d) {
-            //vertical position centered on each circle
-            return y(d.population) + 5;
-        });
+        //add background States to map for fill
+        var bgstates = map.append("path")
+            .datum(statesTopo)
+            .attr("class", "bgstates")
+            .attr("d", path);
 
-    //first line of label
-    var nameLine = labels.append("tspan")
-        .attr("class", "nameLine")
-        .attr("x", function (d, i) {
-            //horizontal position to the right of each circle
-            return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
-        })
-        .attr("dy", "-5") //vertical offset
-        .text(function (d) {
-            return d.city;
-        });
+        //add ARTCC Centers to map
+        var center = map.selectAll(".centers")
+            .data(centersTopo)
+            .enter()
+            .append("path")
+            .attr("class", function (d) {
+                return "centers " + d.properties.artcc;
+            })
+            .attr("d", path);
 
-    var format = d3.format(",");
+        //add States overlay to map
+        var states = map.append("path")
+            .datum(statesTopo)
+            .attr("class", "states")
+            .attr("d", path);
 
-    //second line of label
-    var popLine = labels.append("tspan")
-        .attr("class", "popLine")
-        .attr("x", function (d, i) {
-            return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
-        })
-        .attr("dy", "15") //vertical offset
-        .text(function (d) {
-            return "Pop. " + format(d.population); //use format generator to format numbers
-        });
-}
+        //add city points overlay to map
+        var points = map.append("path")
+            .datum(pointsTopo)
+            .attr("class", "points")
+            .attr("d", path);
 
+    };
+
+};
